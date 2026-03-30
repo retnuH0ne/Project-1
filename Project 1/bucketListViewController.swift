5ebe1fedca98bc6941ba4c7c00fde3b7e1f2dfef
Hunter Trujillo <megafan.exe@gmail.com>
2026-03-29 17:41:38 -1000

    Tab 3 almost complete.

--- Project 1/bucketListViewController.swift
+++ Project 1/bucketListViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class bucketListViewController: UIViewController {
+    
+    var bucketArray = ["Get fluent in Korean", "Go to Japan", "Cook for myslef", "Go to Evo", "Make a mobile fighting game"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bucketArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bucketCell", for: indexPath)
+        cell.textLabel?.text = bucketArray[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
